714a1901cbf80d2945f629bf7f8d522343bf14d9
KOLEV Stefan <stefan.kolev@ext.ec.europa.eu>
2019-07-10 21:33:10 +0200

    aaa

---
+++ src/test/java/be/stefan/todoservice/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.stefan.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
